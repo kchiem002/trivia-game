98df611405fdeaf8541ab028c4b1b37b6031b006
Kathy Chiem <kchie002@gmail.com>
2019-03-17 10:50:52 -0700

    add time down counter

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
-let correctAnswersCount
-let wrongAnswersCount
-let unansweredCount
+let correctAnswersCount = 0
+let wrongAnswersCount = 0
+let unansweredCount = 0
+let count
 let themeSong = document.querySelector('#theme-song')
 let gameArray = [
     {
@@ -20,11 +21,19 @@ let gameArray = [
 ]
 
 const startGame = _ => {
+    let count = 20
+    //play theme song
     themeSong.play()
+    //question and answer content
     document.querySelector('#question-box').innerHTML = `<h1>${gameArray[0].question}</h1>`    
     document.querySelector('#answer-1').innerHTML = `<h2>${gameArray[0].answer1}</h2>`
     document.querySelector('#answer-2').innerHTML = `<h2>${gameArray[0].answer2}</h2>`
     document.querySelector('#answer-3').innerHTML = `<h2>${gameArray[0].answer3}</h2>`
     document.querySelector('#answer-4').innerHTML = `<h2>${gameArray[0].answer4}</h2>`
-    document.querySelector('#timer-box').innerHTML = `<h3>Time Remaining:</h3>`
+    //timer
+    setInterval(_=> {
+        count--
+        console.log(count)
+        document.querySelector('#timer-box').innerHTML = `<h3>Time Remaining: ${count} seconds</h3>`
+    }, 1000)
 }
