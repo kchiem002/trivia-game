66df136e219a6aa325bb840a76eb82a837bfb70f
Kathy Chiem <kchie002@gmail.com>
2019-03-23 16:49:09 -0700

    fixed timer and added question increment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,7 +22,7 @@ let gameArray = [
         question: "What is Andy Dwyer's band name?",
         answers: ["Mouse Rat", "Fourskin", "Scarecrow Boat", "Just the Tip"],
         correctAnswer: "Mouse Rat",
-        answerImage: `<img id="answer-image" src="./assets/images/mouse-rat.jpg">`    
+        answerImage: `<img id="answer-image" src="./assets/images/mouse-rat.jpg">`
     },
 ]
 let i = 0
@@ -36,10 +36,29 @@ let answers = gameArray[i].answers
 let correctAnswer = gameArray[i].correctAnswer
 let answerImage = gameArray[i].answerImage
 
+//function to get question, function call is in HTML
+const getQuestion = _ => {
+    if (i >= gameArray.length) {
+        document.querySelector('#question-box').innerHTML = ""
+        document.querySelector('#answer-box').innerHTML = ""
+    }
+    else {
+        timer = 16
+        themeSong.play()
+        displayTimer()  
+        question = gameArray[i].question
+        answers = gameArray[i].answers
+        correctAnswer = gameArray[i].correctAnswer
+        answerImage = gameArray[i].answerImage
+        document.querySelector('#question-box').innerHTML = '<h1 id="question">' + question + '</h1>'
+        document.querySelector('#answer-box').innerHTML = '<div><h2 class="answer">' + answers[0] + '</div><div><h2 class="answer">' + answers[1] + '</div><div><h2 class="answer">' + answers[2] + '</div>' + '</div><div><h2 class="answer">' + answers[3] + '</div>'
+    }
+}
 
 //function to start timer
 const displayTimer = _ => {
     myTimer = setInterval(_ => {
+        console.log(timer)
         timer--
         document.querySelector('#timer-box').innerHTML = '<h3 id="timer">Time Remaining: ' + timer + ' seconds'
 
@@ -48,53 +67,36 @@ const displayTimer = _ => {
             document.querySelector('#question-box').innerHTML = '<h1> Too slow. The correct answer is ' + correctAnswer
             document.querySelector('#answer-box').innerHTML = answerImage
             document.querySelector('#next-question').innerHTML = '<h3 class="next-button">Next Question</h3>'
+            clearInterval(myTimer)
         }
     }, 1000)
 }
 
-//function to display question and answer choices
-const startGame = _ => {
-    themeSong.play()
-    displayTimer()
-    //display question and answer choices
-    document.querySelector('#question-box').innerHTML = '<h1 id="question">' + question + '</h1>'
-    document.querySelector('#answer-box').innerHTML = '<div><h2 class="answer">' + answers[0] + '</div><div><h2 class="answer">' + answers[1] + '</div><div><h2 class="answer">' + answers[2] + '</div>' + '</div><div><h2 class="answer">' + answers[3] + '</div>'
-    checkUserGuess()
-}
-
-
 //function to check whether user guess is correct/incorrect
 const checkUserGuess = _ => {
-    document.addEventListener('click', event => {
         if (event.target.innerHTML === correctAnswer) {
             correctAnswersCount++
-            i++
             document.querySelector('#question-box').innerHTML = '<h1>That is Correct!</h1>'
             document.querySelector('#answer-box').innerHTML = answerImage
             document.querySelector('#next-question').innerHTML = '<h3 class="next-button">Next Question</h3>'
-            clearInterval(myTimer)
-
         }
         else if (event.target.innerHTML !== correctAnswer && event.target.className === 'answer') {
-            i++
             wrongAnswersCount++
             document.querySelector('#question-box').innerHTML = '<h1> Wrong! The correct answer is ' + correctAnswer
             document.querySelector('#answer-box').innerHTML = answerImage
             document.querySelector('#next-question').innerHTML = '<h3 class="next-button">Next Question</h3>'
-            clearInterval(myTimer)
         }
-    })
+        i++
+        clearInterval(myTimer)
 }
 
 document.addEventListener('click', event => {
     if (event.target.className === 'next-button') {
-        startGame()
-        console.log("next question")
+        getQuestion()
         document.querySelector('#next-question').innerHTML = ""
-        clearInterval(timer)
+    }
+    else if (event.target.className === 'answer') {
+        checkUserGuess()
     }
 })
 
-
-//I don't know why my clearInterval function is not working so I couldn't stop the timer from counting down
-//
